4594cbd627465901541a012321bc7001053b2b0e
Sai Okabayashi <saipuck@gmail.com>
2015-10-16 14:17:54 -0400

    Basic package info.

---
+++ R/njdanalytics.R
@@ -0,0 +1,12 @@
+#' njdanalytics: A package for working with data scraped from NHL.com
+#'
+#' The njdanalytics package provides functions to work in the
+#' NJ Devils Hockey Analytics deparment
+#'
+#'  @name njdanalytics
+NULL
+
+.onAttach <- function( libname, pkgname ) {
+  # packageStartupMessage( "" )
+}
+
