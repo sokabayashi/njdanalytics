4b8c42b45c2234314c9247c79fe75993ee54f6e6
Sai Okabayashi <saipuck@gmail.com>
2015-10-19 14:37:53 -0400

    clean up

--- R/njdanalytics.R
+++ R/njdanalytics.R
@@ -9,28 +9,27 @@ NULL
 .onAttach <- function( libname, pkgname ) {
   # packageStartupMessage( "" )
   base_dir     <- "/media/driveb/projects/nhl"
-  vault_dir    <- paste0( base_dir,  "/Vault"    )
-  r_dir        <- paste0( vault_dir, "/R"        )
-  db_dir       <- paste0( r_dir,     "/db"       )
-  fa_dir       <- paste0( r_dir,     "/fa"       )
-  draft_dir    <- paste0( r_dir,     "/draft"    )
-  report_dir   <- paste0( r_dir,     "/report"   )
-  analysis_dir <- paste0( r_dir,     "/analysis" )
-  study_dir    <- paste0( r_dir,     "/study"    )
+  vault_dir    <- paste0( base_dir, "/Vault" )
+  r_dir        <- paste0( vault_dir, "/R" )
 
-  assign( "base_dir",     base_dir,      envir = .GlobalEnv )
-  assign( "vault_dir",    vault_dir,     envir = .GlobalEnv )
-  assign( "r_dir",        r_dir,         envir = .GlobalEnv )
-  assign( "db_dir",       db_dir,        envir = .GlobalEnv )
-  assign( "fa_dir",       fa_dir,        envir = .GlobalEnv )
-  assign( "draft_dir",    draft_dir,     envir = .GlobalEnv )
-  assign( "report_dir",   report_dir,    envir = .GlobalEnv )
-  assign( "analysis_dir", analysis_dir,  envir = .GlobalEnv )
-  assign( "study_dir",    study_dir,     envir = .GlobalEnv )
+  nhl_dir <- list(
+    base     = base_dir,
+    vault    = vault_dir,
+    r        = r_dir,
+    db       = paste0( r_dir,     "/db"       ),
+    fa       = paste0( r_dir,     "/fa"       ),
+    draft    = paste0( r_dir,     "/draft"    ),
+    report   = paste0( r_dir,     "/report"   ),
+    analysis = paste0( r_dir,     "/analysis" ),
+    study    = paste0( r_dir,     "/study"    )
+  )
+
+  assign( "nhl_dir", nhl_dir, envir = .GlobalEnv )
 }
 
 #' Setup a src to our NHL postgreSQL database
 #'
+#' @return An src for connecting to the 'nhl' PostgreSQL database.
 #' @export
 setup_nhl_db <- function() {
   # assign( "nhl_db", src_postgres( dbname = "nhl", user = "postgres", password = "postgres" ), envir = .GlobalEnv )
