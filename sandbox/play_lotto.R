5d3053c388c187dbfa97f9525c8f50167f43f3cd
Sai Okabayashi <saipuck@gmail.com>
2016-03-14 09:54:32 -0400

    5% not 5.5% for 9th

--- sandbox/play_lotto.R
+++ sandbox/play_lotto.R
@@ -1,7 +1,7 @@
 library(dplyr)
 lottery_odds <- data_frame(
   finish = 30:17, # 30th last, 17th just missed playoffs
-  p    = c( 0.2, 0.135, 0.115, 0.095, 0.085, 0.075, 0.065, 0.06, 0.055, 0.035, 0.030, 0.025, 0.020, 0.01 ) # new for 2016 draft
+  p    = c( 0.2, 0.135, 0.115, 0.095, 0.085, 0.075, 0.065, 0.06, 0.05, 0.035, 0.030, 0.025, 0.020, 0.01 ) # new for 2016 draft
 )
 
 num_draws <- 100e3
@@ -21,6 +21,9 @@ results_matrix <- apply( results_df, 2, function(x) {
 rownames( results_matrix ) <- lottery_odds$finish %>% rev()
 
 results_matrix %>% round( 3 )
+
+top_3 <- results_matrix[ ,1:3] %>% rowSums()
+top_3
 
 # expected pick
 results_matrix %*% 1:14
