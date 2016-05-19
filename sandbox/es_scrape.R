75c0947288640f10f2346e51fa30ab934106367b
Sai Okabayashi <saipuck@gmail.com>
2016-05-19 15:55:21 -0400

    updated to Taran's preferences

--- sandbox/es_scrape.R
+++ sandbox/es_scrape.R
@@ -15,39 +15,38 @@ names( summary_table ) <- c(
   "jersey_number",
   "position",
   "player",
-  "G",
-  "A",
-  "P",
+  "g",
+  "a",
+  "p",
   "plus_minus",
-  "PEN",
-  "PIM",
-  "TOI",
+  "pen",
+  "pim",
+  "toi",
   "shifts",
-  "avg TOI",
-  "TOI PP",
-  "TOI SH",
-  "TOI EV",
-  "SOG",
-  "A_B",
-  "MS",
-  "HT",
-  "GV",
-  "TK",
-  "BS",
-  "FW",
-  "FL",
-  "F_pct"
+  "toi_avg",
+  "toi_pp",
+  "toi_sh",
+  "toi_ev",
+  "sog",
+  "a_b",
+  "ms",
+  "ht",
+  "gv",
+  "tk",
+  "bs",
+  "fw",
+  "fl",
+  "f_pct"
 )
 
-table_clean <- summary_table %>% slice(-1:-2) %>% filter( !is.na(position), position != "SHF"  )
-table_converted <- table_clean %>% mutate(
-TOI = time_mmss_to_decimal( TOI ),
-  `avg TOI` = time_mmss_to_decimal( `avg TOI` ),
-  `TOI PP` = time_mmss_to_decimal( `TOI PP` ),
-  `TOI SH` = time_mmss_to_decimal( `TOI SH` ),
-  `TOI EV` = time_mmss_to_decimal( `TOI EV` )
-) %>% mutate_each( funs(str_trim), everything() )
+table_clean <- summary_table %>% slice(-1:-2) %>% filter( !is.na(position), position != "SHF"  ) %>%
+  mutate_each( funs(str_trim), everything() ) %>% as_data_frame()
 
+table_taran <- table_clean %>% select(
+  player, position, jersey_number,
+  toi, toi_pp, toi_sh, sog ) %>%
+  separate( toi, c( "ttoim", "ttois"), sep=":" ) %>%
+  separate( toi_pp, c( "pptoim", "pptois"), sep=":" ) %>%
+  separate( toi_sh, c( "pktoim", "pktois"), sep=":" )
 
-
-write_csv( table_converted, path="sandbox/ES020022.csv" )
+write_csv( table_taran, path="sandbox/ES020022.csv" )
